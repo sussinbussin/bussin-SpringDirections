af5fa7d03a1a3e63d80793753ad544051663e5b3
Renee Ng <reneenbx@gmail.com>
2022-09-27 22:19:45 +0800

    first commit

---
+++ src/main/java/com/bussin/SpringDirections/SpringDirectionsApplication.java
@@ -0,0 +1,13 @@
+package com.bussin.SpringDirections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDirectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDirectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bussin/SpringDirections/SpringDirectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bussin.SpringDirections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDirectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
